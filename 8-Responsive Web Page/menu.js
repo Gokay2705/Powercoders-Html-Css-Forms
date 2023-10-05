4301e8e4c6397ddefda2bca7b89ba8d090abd5bc
M.Goekche AYDIN <63980772+Gokay2705@users.noreply.github.com>
2023-10-05 16:59:25 +0200

    css

--- 8-Responsive Web Page/menu.js
+++ 8-Responsive Web Page/menu.js
@@ -4,7 +4,11 @@ function openNav() {
   document.getElementById("main").style.marginLeft = "250px";
 }
 
+
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+
+
