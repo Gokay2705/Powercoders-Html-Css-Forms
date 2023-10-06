9c6162b555f8d4f857533670841a4b335f01acf4
M.Goekche AYDIN <63980772+Gokay2705@users.noreply.github.com>
2023-10-06 19:27:47 +0200

    js

--- 8-Responsive Web Page/menu.js
+++ 8-Responsive Web Page/menu.js
@@ -1,9 +1,7 @@
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
-
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
@@ -15,7 +13,7 @@ var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+  dropdown[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
@@ -25,5 +23,3 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
-
-
